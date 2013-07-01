363d54f4d947d9db10d9eba7bd71b19ce97345f4
th3mon <p.szelenberger@gmail.com>
2013-07-02 00:32:14 +0200

    CHANGE: Nice code WORKS!!!

--- views/widget.php
+++ views/widget.php
@@ -1,2 +1,14 @@
 <!-- This file is used to markup the public-facing widget. -->
-<h1>widget.php test</h1>
+<?php
+    if (!empty($title)) {
+?>
+        <h2><?php echo $title; ?></h2>
+<?php
+        }
+
+        if (!empty($link_url) && !empty($image_url)) {
+?>
+        <a href="<?php echo $link_url; ?>"><img src="<?php echo $image_url; ?>" /></a>
+<?php
+        }
+?>
