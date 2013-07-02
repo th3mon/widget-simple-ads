741e7d642023d84cee0beb01355b09af34f41dac
th3mon <p.szelenberger@gmail.com>
2013-07-02 20:13:41 +0200

    CHANGE: correct code for generating widget HTML

--- views/widget.php
+++ views/widget.php
@@ -1,14 +1,12 @@
 <!-- This file is used to markup the public-facing widget. -->
 <?php
     if (!empty($title)) {
-?>
-        <h2><?php echo $title; ?></h2>
-<?php
-        }
+        echo $before_title . $title . $after_title;
+    }
 
-        if (!empty($link_url) && !empty($image_url)) {
+    if (!empty($link_url) && !empty($image_url)) {
 ?>
         <a href="<?php echo $link_url; ?>"><img src="<?php echo $image_url; ?>" /></a>
 <?php
-        }
+    }
 ?>
